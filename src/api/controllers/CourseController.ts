a6787dd3ac3e62d1928add0735d1774ac9b4d34b
s-li1 <stevenli8892@hotmail.com.au>
2021-08-01 09:31:38 +1000

    service call is wrapped in try block

--- src/api/controllers/CourseController.ts
+++ src/api/controllers/CourseController.ts
@@ -2,11 +2,15 @@ import { Request, Response } from 'express';
 const CourseService = require('../services/CourseService');
 
 const get = async(req: Request, res: Response) => {
-    const { name } = req.params;
-    const result = await CourseService.getSubjects(name);
-    res.json({
-        data: result
-    });
+    try {
+        const { name } = req.params;
+        const result = await CourseService.getSubjects(name);
+        res.json({
+            data: result
+        });
+    } catch(error) {
+        console.log(error);
+    }
 }
 
 module.exports = {
