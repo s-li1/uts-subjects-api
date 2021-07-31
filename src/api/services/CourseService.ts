015dcb7014cfce611959b32f1b339a5f09c72ea9
s-li1 <stevenli8892@hotmail.com.au>
2021-08-01 09:32:38 +1000

    removed data html console log

--- src/api/services/CourseService.ts
+++ src/api/services/CourseService.ts
@@ -3,7 +3,6 @@ const Helper = require('../helpers/helper');
 const getSubjects = async(course: string) => {
     try {
         const data = await Helper.fetchHtmlFromUrl(`https://www.handbook.uts.edu.au/${course}/lists/alpha.html`);
-        console.log(data.html());
         return data.html();
     }
     catch(error) {
