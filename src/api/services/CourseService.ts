3c8a251e9e70702c637806cf0ab7d36434bb7645
s-li1 <stevenli8892@hotmail.com.au>
2021-08-06 14:15:34 +1000

    fixedd spacing

--- src/api/services/CourseService.ts
+++ src/api/services/CourseService.ts
@@ -2,19 +2,24 @@ import { fetchHtmlFromUrl } from "../helpers/helper";
 
 const getSubjects = async(course: string): Promise<any> => {
     try {
-        const $ = await fetchHtmlFromUrl(`https://www.handbook.uts.edu.au/${course}/lists/alpha.html`);
+        const $ = await fetchHtmlFromUrl(`https://www.handbook.uts.edu.au/${course}/lists/alpha.html`); 
+        
         const subjectNames = $(".ie-images").contents().filter((i ,el) => {
             return el.type === 'text';
         })
         .text().trim().replace(/(\r\n|\n|\r)/gm, ".").split(".");
+
         let subjectAnchorLinks: any[] = [];
+
         $(".ie-images").find('a').each((i, el) => {
             subjectAnchorLinks.push({
                 id: $(el).text(),
                 link: $(el).attr('href')
             });
         });
+
         const subjectLinksAndIDs = subjectAnchorLinks.filter(subject => !isNaN(subject.id));
+
         return subjectLinksAndIDs.map((subject, index) => {
             return {
                 ...subject,
