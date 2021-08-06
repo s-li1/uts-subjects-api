8c9fc6f0425e474e9ff62012904ccbfb647f858c
s-li1 <stevenli8892@hotmail.com.au>
2021-08-06 12:28:41 +1000

    fetch subject names

--- src/api/services/CourseService.ts
+++ src/api/services/CourseService.ts
@@ -2,8 +2,12 @@ import { fetchHtmlFromUrl } from "../helpers/helper";
 
 const getSubjects = async(course: string): Promise<any> => {
     try {
-        const data = await fetchHtmlFromUrl(`https://www.handbook.uts.edu.au/${course}/lists/alpha.html`);
-        return data.html();
+        const $ = await fetchHtmlFromUrl(`https://www.handbook.uts.edu.au/${course}/lists/alpha.html`);
+        const subjectNames = $(".ie-images").contents().filter((i ,el) => {
+            return el.type === 'text';
+        })
+        .text().trim().replace(/^\s+|\s+$|\s+(?=\s)/g,'').replace(/(\r\n|\n|\r)/gm, ',').split(',');
+        return subjectNames;
     }
     catch(error) {
         console.log(error);
