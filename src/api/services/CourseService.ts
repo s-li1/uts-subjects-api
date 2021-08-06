92069692be14bf08f1e04f0b7351da65fa4e6be9
s-li1 <stevenli8892@hotmail.com.au>
2021-08-06 17:51:46 +1000

    fix

--- src/api/services/CourseService.ts
+++ src/api/services/CourseService.ts
@@ -4,19 +4,17 @@ const getSubjects = async(course: string): Promise<any> => {
     try {
         const $ = await fetchHtmlFromUrl(`https://www.handbook.uts.edu.au/${course}/lists/alpha.html`); 
         
-        const subjectNames = $(".ie-images").contents().filter((i ,el) => {
-            return el.type === 'text';
-        })
+        const subjectNames = $(".ie-images").contents().filter((i, el) => el.type === 'text')
         .text().trim().replace(/(\r\n|\n|\r)/gm, ".").split(".");
 
         let subjectLinksAndIDs: any[] = [];
 
-        $(".ie-images").find('a').each((i, el) => {
+        $(".ie-images").find("a").each((i, el) => {
             const id = $(el).text();
             if (!isNaN(parseInt(id))) {
                 subjectLinksAndIDs.push({
                     id: id,
-                    link: $(el).attr('href')
+                    link: $(el).attr("href")
                 });
             }
         });
