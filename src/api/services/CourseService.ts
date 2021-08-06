48786b7fe532679fa33eb9c8552dd81fe85ca6d9
s-li1 <stevenli8892@hotmail.com.au>
2021-08-06 14:10:52 +1000

    fixed fetching subject names

--- src/api/services/CourseService.ts
+++ src/api/services/CourseService.ts
@@ -6,7 +6,7 @@ const getSubjects = async(course: string): Promise<any> => {
         const subjectNames = $(".ie-images").contents().filter((i ,el) => {
             return el.type === 'text';
         })
-        .text().trim().replace(/^\s+|\s+$|\s+(?=\s)/g,'').replace(/(\r\n|\n|\r)/gm, ',').split(',');
+        .text().trim().replace(/(\r\n|\n|\r)/gm, ".").split(".");
         return subjectNames;
     }
     catch(error) {
